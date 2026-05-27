340c6aecab9930e9d50d8f7a03df1523dff7d634
24096ao <24096ao@hvhs.school.nzgit switch main>
2026-05-27 12:30:11 +1200

    doing cart + menu

---
+++ js/cartScript.js
@@ -0,0 +1,27 @@
+/****************************
+cartScript.js
+****************************/
+console.log("Running cartScript.js")
+
+
+// variables
+const OUTPUT = document.getElementById("itemInfo");
+/****************************
+Main code
+****************************/
+console.log("hello world!");
+
+
+/****************************
+functions
+****************************/
+function addedItem(_product, _price) {
+    console.log("you added " + _product);
+    console.log(_product + "costs: " + _price);
+}
+
+
+/****************************
+end of code
+****************************/
+
---
+++ js/menuScript.js
@@ -0,0 +1,60 @@
+/****************************
+menuScript.js
+****************************/
+console.log("Running menuScript.js")
+
+
+// variables
+let EnglishBreakfast = {
+    name: "english breakfast",
+    price: 20
+};
+let waffle = {
+    name: "Waffle",
+    price: 13
+};
+let pancake = {
+    name: "Pancake",
+    price: 13
+};
+let bagle = {
+    name: "bagle",
+    price: 15
+};
+let sandwich = {
+    name: "sandwich",
+    price: 10
+};
+
+let espresso = 2;
+let doubleEspresso = 2;
+let macchiato = 2;
+let ristretto = 2;
+let longBlack = 2;
+let caffèLatte = 2;
+let cappuccino = 2;
+let flatWhite = 2;
+let iceCoffee = 2;
+let hotChocolate = 1.50;
+let berrySmoothie = 3;
+let feijoaSmoothie = 3.50;
+let water = 1;
+let appleJuice = 2;
+let orangeJuice = 2.30;
+
+/****************************
+Main code
+****************************/
+console.log("hello world!")
+
+
+/****************************
+functions
+****************************/
+
+
+
+/****************************
+end of code
+****************************/
+
